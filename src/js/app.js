24c4db90c2db1ded416a852e6f44d46833c3a8a7
erinkahn <erin.kahn@atlanta.creativecircus.edu>
2018-02-19 11:01:17 -0500

    api key

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-const GIPHY_API_BASE = ""; 
+
+//API Key : bhh0KO5k7BBVEyB1kfKqEHsZNozI68e5
+const GIPHY_API_BASE = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=bhh0KO5k7BBVEyB1kfKqEHsZNozI68e5"; 
 
 const router = new VueRouter({
 	routes: [
@@ -14,10 +16,12 @@ var app = new Vue({
 	el: "#app",
 	router: router,
 	data: {
-		subject: "",
+		searchTerm: "",
 		giphys: [],
 	},
 	methods: {
 
 	},
 })
+
+
